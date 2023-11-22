4f5d8ef1b1e2c9f5c8005a3f65e92f5a4cb0743d
HelioLucio <helio.lucio.jr@hotmail.com>
2023-11-22 16:41:19 -0300

    entry button text turns to white when pressed

--- src/components/Login/EnterButton.tsx
+++ src/components/Login/EnterButton.tsx
@@ -1,19 +1,45 @@
-import { Text, TouchableOpacity } from 'react-native'
-import { BaseSyntheticEvent } from 'react'
+import { Text, TouchableHighlight, ViewStyle } from 'react-native'
+import { BaseSyntheticEvent, useState } from 'react'
 
 interface EnterButtonProps {
   handleLogin: (e?: BaseSyntheticEvent<object>) => Promise<void>
 }
 
 export function EnterButton({ handleLogin }: EnterButtonProps) {
+  const [isPressed, setIsPressed] = useState(false)
+
+  const handleShowUnderlay = () => {
+    setIsPressed(true)
+  }
+
+  const handleHideUnderlay = () => {
+    setIsPressed(false)
+  }
+
+  const buttonStyle: ViewStyle = {
+    height: 40,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    backgroundColor: isPressed ? '#EC8B00' : '#9CA3AF', // Change these colors as needed
+  }
+
+  const textStyle = {
+    color: isPressed ? 'white' : 'black',
+  }
+
   return (
-    <TouchableOpacity
-      className="flex h-10 w-full items-center justify-center rounded-full bg-gray-400 active:bg-brqOrange"
+    <TouchableHighlight
+      style={buttonStyle}
+      activeOpacity={1}
+      underlayColor="orange" // Change this to the desired color when pressed
       onPress={handleLogin}
-      activeOpacity={0.8}
+      onShowUnderlay={handleShowUnderlay}
+      onHideUnderlay={handleHideUnderlay}
       testID="EntryButton"
     >
-      <Text className="text-brqNeutral">Entrar</Text>
-    </TouchableOpacity>
+      <Text style={textStyle}>Entrar</Text>
+    </TouchableHighlight>
   )
 }
